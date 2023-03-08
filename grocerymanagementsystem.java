d42d59611d2af1b1398d99b6f486e53e131640d7
Shashi2682 <125998499+Shashi2682@users.noreply.github.com>
2023-03-08 22:46:12 +0530

    Update grocerymanagementsystem.java

--- grocerymanagementsystem.java
+++ grocerymanagementsystem.java
@@ -5,15 +5,22 @@ class product
     Scanner sc = new Scanner(System.in);
     int n;
     String[] name;
+    String[] addname;
     int quantity[];
+    int addquantity[];
     int price[];
+    int addprice[];
     void numberofproducts()
     {
         System.out.println("Enter number of products");
         n = sc.nextInt();
         name=new String[n];
-        quantity=new int[n];
+        addname=new String[n];
+        quantity = new int[n];
+        addquantity = new int[n];
         price = new int[n];
+        addprice = new int[n];
+
     }
     void products()
    {
@@ -25,7 +32,7 @@ class product
        for (int i=0;i<n;i++)
        {
            System.out.println("enter quantity for "+name[i] + ":");
-           quantity[i]= sc.nextInt();
+            quantity[i]= sc.nextInt();
        }
        for (int i=0;i<n;i++)
        {
@@ -37,12 +44,53 @@ class product
    {
        for (int i=0;i<n;i++)
        {
-           System.out.println(i+")"+"Product::"+"Quantity::"+"Price");
+           System.out.println((i+1)+")"+"Product::"+"Quantity::"+"Price");
            System.out.println(name[i]+"::"+quantity[i]+"::"+price[i]);
        }
    }
+   int choice;
+    int s;
+   void addproduct()
+   {
+       System.out.println("Do you wish to add product if yes then enter '1' else enter '0' ");
+       choice = sc.nextInt();
+       if(choice==1 || choice==0)
+       {
+           if (choice == 1) {
+               System.out.println("number of products you want to increase");
+               s = sc.nextInt();
+               addname = new String[(n + s)+2];
+               System.out.println(addname.length);
+               addquantity = new int[(n + s)+2];
+               addprice = new int[(n + s)+2];
+               int t = n+s;
+               System.arraycopy(this.name, 0, addname, 0, name.length);
+               System.arraycopy(this.quantity, 0, addquantity, 0, quantity.length);
+               System.arraycopy(this.price, 0, addprice, 0, price.length);
+               System.out.println("enter the product");
+               addname[t-1] = sc.next();
+               System.out.println("enter the quantity");
+               addquantity[t-1] = sc.nextInt();
+               System.out.println("enter the price");
+               addprice[t-1] = sc.nextInt();
+           }
+       }
+   }
+   void output3()
+   {
+      if(choice==1)
+      {
+          for (int i=0;i<n+s;i++)
+          {
+              System.out.println("your products with increased items are");
+              System.out.println((i+1)+")"+"Product::"+"Quantity::"+"Price");
+              System.out.println(addname[i]+"::"+addquantity[i]+"::"+addprice[i]);
+          }
+      }
+   }
 
 }
+
 class mainmethod extends product
 {
     public static void main(String[] args)
@@ -58,6 +106,8 @@ class mainmethod extends product
         p.numberofproducts();
         p.products();
         p.output();
+        p.addproduct();
+        p.output3();
         if(g==1)
         {
             System.out.println("Thank you,Mr." + Name + " for shopping with us");
@@ -66,6 +116,8 @@ class mainmethod extends product
         {
             System.out.println("Thank you,Mrs." + Name + " for shopping with us");
         }
+        
+
 
 
 
